45de256cb38c0316f7453c102c589e41a01a5dcf
George Goering <ggoeringgg@gmail.com>
2017-04-03 17:18:57 -0600

    Beginning prepend function

---
+++ main.js
@@ -0,0 +1,29 @@
+
+
+var $saveButton = $('.save-button').on('click', function() {
+  var $title = $('#title').val()
+  var $body = $('#body').val()
+  var $id = Date.now()
+  var newIdea = new CardObject($id, $title, $body, $quality)
+  prependCard(newIdea)
+})
+
+function prependCard(newIdea) {
+  $('.card-container').prepend(`<article class="idea-card">
+      <button type="button" class="delete"></button>
+      <h2>${$title}</h2>
+      <p>${$body}</p>
+      <div class="quality-container">
+        <button type="button" class="up-vote"></button>
+        <button type="button" class="down-vote"></button>
+        <p class="idea-quality"><span class="quality-font">quality:</span>${$quality}</p>
+      </div>
+    </article>`)
+}
+
+function CardObject(id, title, body, quality) {
+  this.id = id
+  this.title = title
+  this.body = body
+  this.quality = "swill"
+}
