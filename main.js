b82d2b0bec9b15a18233003f234bc1adc13a10b5
Evan Miller <evanmillerny@gmail.com>
2017-04-12 20:08:41 -0600

    clean up CSS, JS, HTML with proper names. Reused and sorted functions in JS.

--- main.js
+++ main.js
@@ -1,21 +1,23 @@
 $(document).ready(prependOnStart)
 
 $('.save-button').on('click', saveButton)
-  .on('click', saveButtonDisable);
-$('#title, #body').on('input', saveButtonEnable);
+                  .on('click', saveButtonDisable);
+$('#title, #task').on('input', saveButtonEnable);
 $('.card-container').on('click', '.delete', deleteCard);
 $('.card-container').on('blur', 'h2', titlePersist);
 $('.card-container').on('blur', 'p', bodyPersist);
 $('.card-container').on('click', '.up-vote', increaseIndex);
 $('.card-container').on('click', '.down-vote', decreaseIndex);
-$('#search').on('keyup', searchCards);
+$('#filter').on('keyup', searchCards);
+$('.card-container').on('keypress', "h2, #p-body", something)
+$("#title, #task").keypress(enterKeySave)
 
 /*=======================================
 >>>>>>>>  ON START  <<<<<<<<
 ========================================*/
 function clearInputs() {
   $('#title').val('')
-  $('#body').val('')
+  $('#task').val('')
 }
 
 function prependOnStart() {
@@ -23,7 +25,6 @@ function prependOnStart() {
     prependCard(JSON.parse(localStorage.getItem(localStorage.key(i))));
   }
 }
-
 /*=======================================
 >>>>>>>> OBJECT CONSTRUCTOR FUNCTION <<<<<<<<
 ========================================*/
@@ -41,7 +42,7 @@ function prependCard(newIdea) {
     `<article class="idea-card" id="${newIdea.id}" data-index="${newIdea.index}" data-list="${newIdea.qualities}">
       <button type="button" class="delete"></button>
       <h2 contenteditable="true">${newIdea.title}</h2>
-      <p contenteditable="true">${newIdea.body}</p>
+      <p id="p-body" contenteditable="true">${newIdea.body}</p>
       <div class="quality-container">
         <button type="button" class="up-vote"></button>
         <button type="button" class="down-vote"></button>
@@ -50,80 +51,57 @@ function prependCard(newIdea) {
     </article>`)
 }
 
-
 /*=======================================
 >>>>>>>>  IMPORTANCE VALUE CHANGER  <<<<<<<<
 ========================================*/
 function increaseIndex() {
   var cardID = ($(this).closest('.idea-card').attr('id'))
   var data = getObjectData(cardID)
-  if (data[2] == data[1].length - 1) {
-    return;
-  }
+  if (data[2] == data[1].length - 1) {return;}
   data[2]++;
   data[0].quality = data[1][data[2]]
   data[0].index = data[2]
-  localStorage.setItem(cardID, JSON.stringify(data[0]))
+  setItemLocalStorage(cardID, data[0])
   $(this).siblings("p").children(".rating").text(data[1][data[2]])
-
 }
 
 function decreaseIndex() {
   var cardID = ($(this).closest('.idea-card').attr('id'))
   var data = getObjectData(cardID)
-  if (data[2] == 0) {
-    return;
-  }
+  if (data[2] == 0) { return;}
   data[2]--;
   data[0].quality = data[1][data[2]]
   data[0].index = data[2]
-  localStorage.setItem(cardID, JSON.stringify(data[0]))
+  setItemLocalStorage(cardID, data[0])
   $(this).siblings("p").children(".rating").text(data[1][data[2]])
-}
-
-var getObjectData = function(cardID) {
-  var parsedObject = JSON.parse(localStorage.getItem(cardID))
-  var qualityList = parsedObject.qualities
-  var index = parsedObject.index
-  var quality = parsedObject.quality
-  return [parsedObject, qualityList, index, quality]
 }
 
 /*=======================================
 >>>>>>>>  LOCAL STORAGE  <<<<<<<<
 ========================================*/
+function titlePersist() {
+  var cardID = $(this).closest('.idea-card').attr('id')
+  var newText = $(this).text()
+  var arrayTitleKey = "title"
+  var data = getObjectData(cardID)
+  data[0].title = newText;
+  setItemLocalStorage(cardID, data[0])
+}
 
 function bodyPersist() {
   var cardID = $(this).closest('.idea-card').attr('id')
-  var h2Body = $(this).text()
-  var retrieveObject = localStorage.getItem(cardID)
-  var parsedObject = JSON.parse(retrieveObject)
-  parsedObject.body = h2Body;
-  var stringifiedObject = JSON.stringify(parsedObject)
-  localStorage.setItem(cardID, stringifiedObject)
-}
-
-function retrieveObjectStorage(cardID, text, key) {
-  var parsedObject = JSON.parse(localStorage.getItem(cardID))
-  parsedObject[key] = text;
-  localStorage.setItem(cardID, JSON.stringify(parsedObject))
-}
-
-/*=======================================
->>>>>>>>  EVENT LISTENER TRIGGERS  <<<<<<<<
-========================================*/
-
-function saveButton() {
-  var $title = $('#title').val()
-  var $body = $('#body').val()
-  var newIdea = new CardObject($title, $body)
-  clearInputs()
-  storeIdea(newIdea)
-  prependCard(newIdea)
+  var newText = $(this).text()
+  var data = getObjectData(cardID)
+  data[0].body = newText;
+  setItemLocalStorage(cardID, data[0])
 }
 
 function storeIdea(newIdea) {
   localStorage.setItem(newIdea.id, JSON.stringify(newIdea))
+}
+
+function setItemLocalStorage(id, object) {
+  localStorage.setItem(id, JSON.stringify(object))
 }
 
 function deleteCard() {
@@ -132,23 +110,27 @@ function deleteCard() {
   localStorage.removeItem(cardID);
 }
 
-function titlePersist() {
-  var cardID = $(this).closest('.idea-card').attr('id')
-  var text = $(this).text()
-  var arrayTitleKey = "title"
-  retrieveObjectStorage(cardID, text, arrayTitleKey)
+var getObjectData = function(cardID) {
+  var parsedObject = JSON.parse(localStorage.getItem(cardID))
+  return [parsedObject, parsedObject.qualities, parsedObject.index, parsedObject.quality]
 }
 
-function bodyPersist() {
-  var cardID = $(this).closest('.idea-card').attr('id')
-  var text = $(this).text()
-  var arrayBodyKey = "body"
-  retrieveObjectStorage(cardID, text, arrayBodyKey)
+/*=======================================
+>>>>>>>>  EVENT LISTENER TRIGGERS  <<<<<<<<
+========================================*/
+
+function saveButton() {
+  var $title = $('#title').val()
+  var $body = $('#task').val()
+  var newIdea = new CardObject($title, $body)
+  clearInputs()
+  storeIdea(newIdea)
+  prependCard(newIdea)
 }
 
 function saveButtonEnable() {
   var titleInput = $('#title').val();
-  var bodyInput = $('#body').val();
+  var bodyInput = $('#task').val();
   if (titleInput !== "" && bodyInput !== "") {
     $('.save-button').attr("disabled", false)
   }
@@ -156,7 +138,7 @@ function saveButtonEnable() {
 
 function saveButtonDisable() {
   var titleInput = $('#title').val();
-  var bodyInput = $('#body').val();
+  var bodyInput = $('#task').val();
   if (titleInput === "" && bodyInput === "") {
     $('.save-button').attr("disabled", true)
   }
@@ -174,19 +156,19 @@ function searchCards() {
   })
 }
 
-$("#title, #body").keypress(function(e) {
+function enterKeySave() {
   var titleInput = $('#title').val();
-  var bodyInput = $('#body').val();
-  if (titleInput !== "" && bodyInput !== "" && e.which == 13) {
+  var bodyInput = $('#task').val();
+  if (titleInput !== "" && bodyInput !== "" && event.which == 13) {
     $('.save-button').click()
   }
-});
+};
 
-$('.card-container').on('keypress', "h2, #p-body", function(event) {
+function something() {
   if (event.keyCode == 13 && event.shiftKey) {
     alert('Enter + shift pressed');
   } else if (event.keyCode == 13) {
     event.preventDefault();
     $("h2, #p-body").blur()
   }
-});
+};
